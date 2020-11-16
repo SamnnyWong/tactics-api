e72d12ca51b83c8ce47377e0742b6c17fa7f1f89
SamnnyWong <hi.min3@gmail.com>
2020-11-16 15:08:51 +0800

    reset repo

--- collector/web-crawler/dateConvert.js
+++
@@ -1,8 +0,0 @@
-import handler from "./libs/handler-lib";
-
-export const main = handler(async (event, context) => {
-    const timeString = '2020-10-30T21:34:54.653Z';
-    var newDate = new Date(timeString).toLocaleString("en-US", {timeZone: "Asia/Shanghai"});
-    console.log(newDate);
-    return newDate
-});
